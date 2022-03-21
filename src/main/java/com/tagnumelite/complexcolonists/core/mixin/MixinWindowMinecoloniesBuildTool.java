7758faefe6b92463df8740b34be79b7f02157188
Tagan Hoyle <tagnum.elite@gmail.com>
2022-03-21 18:21:45 +0200

    Add mixin for WindowMinecoloniesBuildTool

---
+++ src/main/java/com/tagnumelite/complexcolonists/core/mixin/MixinWindowMinecoloniesBuildTool.java
@@ -0,0 +1,50 @@
+package com.tagnumelite.complexcolonists.core.mixin;
+
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import com.minecolonies.api.colony.buildings.registry.BuildingEntry;
+import com.minecolonies.api.colony.buildings.registry.IBuildingRegistry;
+import com.minecolonies.api.util.InventoryUtils;
+import com.minecolonies.coremod.client.gui.WindowMinecoloniesBuildTool;
+import com.ldtteam.structurize.client.gui.WindowBuildTool;
+import com.ldtteam.structurize.management.StructureName;
+
+@Mixin(value = WindowMinecoloniesBuildTool.class, remap = false)
+public abstract class MixinWindowMinecoloniesBuildTool extends WindowBuildTool {
+    public MixinWindowMinecoloniesBuildTool(@Nullable BlockPos pos, String folder, int rotation, int groundstyle) {
+        super(pos, folder, rotation, groundstyle);
+    }
+
+    /**
+     * Overwrite the hasMatchingBlock function because the base function crashes when it tries getBuildingBlock from a
+     * null BuildingEntry.
+     *
+     * @author TagnumElite
+     * @reason The base method crashes because it tries to get a Block from a null BuildingEntry.
+     */
+    @Overwrite
+    @Override
+    public boolean hasMatchingBlock(@NotNull PlayerInventory inventory, String hut) {
+        final String name = hut.equals("citizen") ? "home" : hut;
+        return InventoryUtils.hasItemInProvider(inventory.player,
+                item -> {
+                    BuildingEntry buildingReg = IBuildingRegistry.getInstance()
+                                                                 .getValue(
+                                                                         new ResourceLocation(
+                                                                                 item.getItem()
+                                                                                     .getRegistryName()
+                                                                                     .getNamespace(),
+                                                                                 name));
+                    return item.getItem() instanceof BlockItem
+                           && StructureName.HUTS.contains(hut) && buildingReg != null
+                           && ((BlockItem) item.getItem()).getBlock() == buildingReg.getBuildingBlock();
+                });
+    }
+}
