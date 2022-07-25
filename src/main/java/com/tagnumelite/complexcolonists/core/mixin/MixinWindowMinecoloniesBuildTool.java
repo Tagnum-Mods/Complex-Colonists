72ab00df5c20a0680b8a91e03b76c5a9cbd0013e
Tagan Hoyle <tagnum.elite@gmail.com>
2022-07-25 15:24:03 +0200

    Fix Mixin for the BuildTool
    Cleanup the code and prevent crashes to to null items.

--- src/main/java/com/tagnumelite/complexcolonists/core/mixin/MixinWindowMinecoloniesBuildTool.java
+++ src/main/java/com/tagnumelite/complexcolonists/core/mixin/MixinWindowMinecoloniesBuildTool.java
@@ -43,16 +43,21 @@ public abstract class MixinWindowMinecoloniesBuildTool extends WindowBuildTool {
 
         Predicate<ItemStack> predicate = (itemStack) -> {
             final Item             item         = itemStack.getItem();
-            final String           namespace    = (item.getRegistryName() != null) ? item.getRegistryName()
-                                                                                         .getNamespace() : Constants.MOD_ID;
-            final ResourceLocation registryName = new ResourceLocation(namespace, name);
-
             final boolean isBlockItem = item instanceof BlockItem;
             final boolean isHut       = StructureName.HUTS.contains(hut);
-            final boolean buildingBlockExists = isBlockItem && ((BlockItem) itemStack.getItem()).getBlock() == buildingRegistry.getValue(
-                    registryName).getBuildingBlock();
+            boolean isBuildingBlock = false;
+            
+            if (isBlockItem && isHut) {
+                final String           namespace    = (item.getRegistryName() != null) ? item.getRegistryName()
+                                                                                             .getNamespace() : Constants.MOD_ID;
+                final ResourceLocation registryName = new ResourceLocation(namespace, name);
 
-            return isBlockItem && isHut && buildingBlockExists;
+                @Nullable BuildingEntry buildingEntry = buildingRegistry.getValue(registryName);
+
+                if (buildingEntry != null) isBuildingBlock = ((BlockItem) itemStack.getItem()).getBlock() == buildingEntry.getBuildingBlock();
+            }
+
+            return isBlockItem && isHut && isBuildingBlock;
         };
 
         return InventoryUtils.hasItemInProvider(inventory.player, predicate);
