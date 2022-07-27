2c4319e9f5099761cf98f127630c4b1d4a315c0b
Tagan Hoyle <tagnum.elite@gmail.com>
2022-07-27 09:52:08 +0200

    Create DataGenerator

---
+++ src/main/java/com/tagnumelite/complexcolonists/datagen/CCDataGenerators.java
@@ -0,0 +1,22 @@
+package com.tagnumelite.complexcolonists.datagen;
+
+import com.tagnumelite.complexcolonists.ComplexColonists;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = ComplexColonists.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class CCDataGenerators {
+    private CCDataGenerators() {}
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator      generator          = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        if (event.includeClient()) {
+        }
+    }
+}
